94a6ef1127ea7a141344acbbedd49ba4efd4a4c3
Weverton Ferreira <contato.wevertonfelipe@gmail.com>
2022-11-09 19:13:44 -0300

    Added new API project

---
+++ src/Task.Management.Api/Controllers/UsersController.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Task.Management.API.Controllers
+{
+    [Route("api/users")]
+    public class UsersController : ControllerBase
+    {
+        public UsersController()
+        {
+
+        }
+
+        [HttpGet("{userId}")]
+        public async Task<IActionResult> GetbyId(string userId)
+        {
+            return Ok();
+        }
+    }
+}
