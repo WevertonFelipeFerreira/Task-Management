2c50fcaffd565ecb1ddd8cc77cf328f84a2e9b17
Weverton Ferreira <contato.wevertonfelipe@gmail.com>
2022-11-09 19:39:31 -0300

    Added routes

--- src/Task.Management.Api/Controllers/UsersController.cs
+++ src/Task.Management.Api/Controllers/UsersController.cs
@@ -10,8 +10,32 @@ namespace Task.Management.API.Controllers
 
         }
 
+        [HttpPost("login")]
+        public async Task<IActionResult> Login(string userId)
+        {
+            return Ok();
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Post()
+        {
+            return Ok();
+        }
+
         [HttpGet("{userId}")]
         public async Task<IActionResult> GetbyId(string userId)
+        {
+            return Ok();
+        }
+
+        [HttpPut]
+        public async Task<IActionResult> Update()
+        {
+            return Ok();
+        }
+
+        [HttpDelete]
+        public async Task<IActionResult> Delete()
         {
             return Ok();
         }
